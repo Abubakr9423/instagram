e7cf62f91520d07483ee35fbe60ef26b3cdb34e5
s <hosimsaidzoda@gmail.com>
2026-01-21 19:52:51 +0500

    wjefiwfjwe

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,13 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
-import home from "./features/home/homeslice"
-import post from "./features/CreatePost/postSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import home from "./features/home/homeslice";
+import messagesReducer from "./features/messages/messagesSlice";
 
 export const store = configureStore({
-    reducer: {
-        counter: home,
-        post: post
-    }
-})
+  reducer: {
+    home: home,
+    messagesApi: messagesReducer,
+  },
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
